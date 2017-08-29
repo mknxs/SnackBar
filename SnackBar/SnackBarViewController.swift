48bb0a3f33a563f4923176eb39e949cae44e1fd6
mkns <mkns@naritoshi-no-MacBook-Pro.local>
2017-08-29 22:29:35 +0900

    SnackBar UI作成

---
+++ SnackBar/SnackBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SnackBarViewController.swift
+//  SnackBar
+//
+//  Created by mkns on 2017/08/29.
+//  Copyright © 2017年 smakino. All rights reserved.
+//
+
+import UIKit
+
+class SnackBarViewController: UIViewController {
+    @IBOutlet weak var snackBarView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var snackBarHeight: NSLayoutConstraint!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
